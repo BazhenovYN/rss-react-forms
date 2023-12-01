6400507cc3837c93b760da19dd77055e71c05c4d
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-12-01 18:48:58 +0100

    feat: update components for working with react hook form

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,10 +1,10 @@
 import {
   ChangeEvent,
   InputHTMLAttributes,
-  FocusEvent,
   useEffect,
   useRef,
   useState,
+  forwardRef,
 } from 'react';
 import styles from './Autocomplete.module.scss';
 
@@ -12,81 +12,84 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   options: string[];
   errorText?: string;
+  onInputChange?: (value: string) => void;
+  value?: string;
 };
 
-function Autocomplete({ label, options, errorText, ...rest }: Props) {
-  const autocompleteRef = useRef<HTMLDivElement | null>(null);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [inputValue, setInputValue] = useState('');
+const Autocomplete = forwardRef<HTMLInputElement, Props>(
+  ({ label, options, errorText, onInputChange, value, ...rest }, ref) => {
+    const autocompleteRef = useRef<HTMLDivElement | null>(null);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    const [inputValue, setInputValue] = useState(value ?? '');
 
-  useEffect(() => {
-    const handleClickOutSide = (event: MouseEvent) => {
-      if (
-        autocompleteRef.current &&
-        !autocompleteRef.current.contains(event.target as Node)
-      ) {
-        setShowSuggestions(false);
+    useEffect(() => {
+      const handleClickOutSide = (event: MouseEvent) => {
+        if (
+          autocompleteRef.current &&
+          !autocompleteRef.current.contains(event.target as Node)
+        ) {
+          setShowSuggestions(false);
+        }
+      };
+      document.addEventListener('mousedown', handleClickOutSide);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutSide);
+      };
+    }, []);
+
+    const suggestions = options.filter((item) =>
+      item.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value);
+      if (onInputChange) {
+        onInputChange(event.target.value);
       }
     };
-    document.addEventListener('mousedown', handleClickOutSide);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutSide);
+
+    const handleSuggestionClick = (suggetion: string) => {
+      setInputValue(suggetion);
+      setShowSuggestions(false);
+      if (onInputChange) {
+        onInputChange(suggetion);
+      }
     };
-  }, []);
 
-  const suggestions = options.filter((item) =>
-    item.toLowerCase().includes(inputValue.toLowerCase())
-  );
-
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleInputBlur = (event: FocusEvent<HTMLInputElement>) => {
-    const query = event.target.value;
-    if (!options.find((item) => item === query)) {
-      setInputValue('');
-    }
-  };
-
-  const handleSuggestionClick = (suggetion: string) => {
-    setInputValue(suggetion);
-    setShowSuggestions(false);
-  };
-
-  return (
-    <div className={styles.autocomplete} ref={autocompleteRef}>
-      <label>
-        {label}
-        <input
-          type="search"
-          value={inputValue}
-          className={errorText ? styles.error : ''}
-          onChange={handleInputChange}
-          onFocus={() => setShowSuggestions(true)}
-          onBlur={handleInputBlur}
-          autoComplete="off"
-          {...rest}
-        />
-      </label>
-      <div className={styles['error-message']}>{errorText}</div>
-      {showSuggestions && (
-        <ul className={styles.suggestions}>
-          {suggestions.map((suggestion) => (
-            <li
-              onClick={() => handleSuggestionClick(suggestion)}
-              key={suggestion}
-            >
-              {suggestion}
-            </li>
-          ))}
-          {suggestions.length === 0 && (
-            <li className={styles.placeholder}>No options...</li>
-          )}
-        </ul>
-      )}
-    </div>
-  );
-}
+    return (
+      <div className={styles.autocomplete} ref={autocompleteRef}>
+        <label>
+          {label}
+          <input
+            type="search"
+            value={inputValue}
+            className={errorText ? styles.error : ''}
+            onChange={handleInputChange}
+            onFocus={() => setShowSuggestions(true)}
+            autoComplete="off"
+            ref={ref}
+            {...rest}
+          />
+        </label>
+        <div className={styles['error-message']}>{errorText}</div>
+        {showSuggestions && (
+          <ul className={styles.suggestions}>
+            {suggestions.map((suggestion) => (
+              <li
+                onClick={() => handleSuggestionClick(suggestion)}
+                key={suggestion}
+              >
+                {suggestion}
+              </li>
+            ))}
+            {suggestions.length === 0 && (
+              <li className={styles.placeholder}>No options...</li>
+            )}
+          </ul>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Autocomplete;
