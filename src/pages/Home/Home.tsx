e7b0e7f9167826e0c79fffb0298869b9128b6851
Iurii Bazhenov <bazhenovyn@gmail.com>
2023-12-03 21:38:37 +0100

    feat: improve form styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ import styles from './Home.module.scss';
 function Home() {
   return (
     <section className={styles.home}>
-      <h1>React Forms</h1>
+      <h1>React. Forms</h1>
       <Link to="/simple-form">Simple form</Link>
       <Link to="/complex-form">Complex form</Link>
       <HistoryList />
